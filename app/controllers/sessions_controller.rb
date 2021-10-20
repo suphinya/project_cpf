0f25acf5b4d1e47d71ba0a6053b7c2604c487ff1
Suphinya Wu <suphinya156165@hotmail.com>
2021-10-20 06:23:12 -0400

    new

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+
+	def create
+		user = User.find_by(username: params[:username])
+
+		if !!user && user.authenticate(params[:password])
+	  		session[:user_id] = user.id
+	  		redirect_to root_path
+		else
+	  		message = "Oop Sorry ! Make sure your username and password are correct !!"
+	  		redirect_to login_path , notice: message
+		end
+  	end
+
+end
