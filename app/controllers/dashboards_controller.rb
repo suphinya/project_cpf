8a201aa3ac5c417bcfd396e5bd8c16d73bf4927a
Suphinya Wu <suphinya156165@hotmail.com>
2021-10-20 09:18:43 -0400

    start login

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,13 @@
 class DashboardsController < ApplicationController
+
+	#before_action :require_login
+
+	def require_login
+		unless @current_user
+			redirect_to login_path
+		end
+	end
+
 	def index
 	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
 
 	def create
-		user = User.find_by(username: params[:username])
+		@user = User.find_by(username: params[:username])
 
-		if !!user && user.authenticate(params[:password])
-	  		session[:user_id] = user.id
+		if @user && @user.authenticate(params[:password])
+	  		session[:user_id] = @user.id
 	  		redirect_to root_path
 		else
 	  		message = "Oop Sorry ! Make sure your username and password are correct !!"
