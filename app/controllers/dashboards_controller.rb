266ecd0da8b275a2d47880d9b681d54e11883c7f
Suphinya Wu <suphinya156165@hotmail.com>
2021-11-11 07:06:13 -0500

    edit acual

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -7,10 +7,6 @@ class DashboardsController < ApplicationController
 		@department_name = params[:id]
 	end
 
-	def date_and_time_info
-		params.require(:actual).permit(:date, :time_in, :time_out)
-	end
-
 	def show
 		@department_name = params[:id]
 		@all_workers = User.where(:position => 'employee', :department1 => @department_name)
