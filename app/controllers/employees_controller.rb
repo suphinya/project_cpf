e88f6a16abbacbea29137dfb42e7481c5431f950
Suphinya Wu <suphinya156165@hotmail.com>
2021-11-12 08:38:51 -0500

    detect plan in employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -5,7 +5,6 @@ class EmployeesController < ApplicationController
 		day = Time.current.strftime("%Y-%m-%d")
 		@date = day.to_time 
 
-		#name_user = current_user.name
 		userid = current_user.id
 		user = User.find(userid)
 		plan = Plan.find_by(:user_id => userid , :date => @date)
@@ -13,32 +12,34 @@ class EmployeesController < ApplicationController
 
 		check_actual = Actual.find_by(:date => @date , :user_id => userid)
 
-		if (params.key?("save_in"))
-			if check_actual == nil
+		if (params.key?("save_in")) && plan != nil
+			if check_actual == nil 
 				user_actual =  Actual.create(:date => @date , :time_in => @time )
 				user.actuals << user_actual
 				plan.actuals << user_actual
+				flash[:notice] = "check in success"
 			else
-				check_actual.update(:time_in => @time)
+				flash[:notice] = "You have check in before !"
 			end	
-			flash[:notice] = "check in success"
 			redirect_to schedule_path
+		elsif (params.key?("save_in"))
+			flash[:notice] = "You don't have any plan !"
 		end
 
-		if (params.key?("save_out"))
-			if check_actual != nil
+		if (params.key?("save_out")) && plan != nil
+			if check_actual != nil 
 				check_actual.update(:time_out => @time )
 				act_out = check_actual.time_out
 				plan_out = plan.time_out
 				@ot = (act_out-plan_out+(12*3600))/(60*60)
 				check_actual.update(:OT => @ot )
+				flash[:notice] = "check out success"
 			else
-				user_actual =  Actual.create(:date => @date , :time_out => @time )
-				user.actuals << user_actual
-				plan.actuals << user_actual
+				flash[:notice] = "You have check out before !"
 			end	
-			flash[:notice] = "check out success"
 			redirect_to schedule_path
+		elsif (params.key?("save_in"))
+			flash[:notice] = "You don't have any plan !"
 		end
 		
 	end
