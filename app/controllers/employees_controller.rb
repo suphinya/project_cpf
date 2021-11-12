24fca4a408d8d5306f5de865ed28839b9e1aa160
Onpinya Phokhahutthakosol <ryukoki2000@gmail.com>
2021-11-12 08:36:34 -0500

    Detect press check in and out

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -15,29 +15,36 @@ class EmployeesController < ApplicationController
 
 		if (params.key?("save_in"))
 			if check_actual == nil
-				user_actual =  Actual.create(:date => @date , :time_in => @time )
-				user.actuals << user_actual
-				plan.actuals << user_actual
+				if plan != nil
+					user_actual =  Actual.create(:date => @date , :time_in => @time )
+					user.actuals << user_actual
+					plan.actuals << user_actual
+					flash[:notice] = "check in success"
+				else
+					flash[:warning] = "You don't have plan for today."
+				end
 			else
-				check_actual.update(:time_in => @time)
-			end	
-			flash[:notice] = "check in success"
+				flash[:warning] = "You have just checked in for today."
+			end
 			redirect_to schedule_path
 		end
 
 		if (params.key?("save_out"))
 			if check_actual != nil
-				check_actual.update(:time_out => @time )
-				act_out = check_actual.time_out
-				plan_out = plan.time_out
-				@ot = (act_out-plan_out+(12*3600))/(60*60)
-				check_actual.update(:OT => @ot )
+				flash[:warning] = "You have just checked out for today."
 			else
-				user_actual =  Actual.create(:date => @date , :time_out => @time )
-				user.actuals << user_actual
-				plan.actuals << user_actual
+				if plan != nil
+					user_actual =  Actual.create(:date => @date , :time_out => @time )
+					user.actuals << user_actual
+					plan.actuals << user_actual
+					act_out = check_actual.time_out
+					plan_out = plan.time_out
+					@ot = (act_out-plan_out+(12*3600))/(60*60)
+					flash[:notice] = "check out success"
+				else
+					flash[:warning] = "You don't have plan for today."
+				end
 			end	
-			flash[:notice] = "check out success"
 			redirect_to schedule_path
 		end
 		
