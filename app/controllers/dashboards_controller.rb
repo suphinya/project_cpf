26fbe493213cbe78fb32d48f9816c613b0ee0859
chanda <11214chanda@gmail.com>
2021-11-14 15:25:52 +0700

    update veiw

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -38,24 +38,59 @@ class DashboardsController < ApplicationController
 		@employee = @users.select{|user| user.position=="employee"}
 		# click to assign shift
 		if (params.key?('ass_button'))
+			@calender = params[:emp][:date]
 			# list of user id (tick checkbox)
 			@uID_list = params[:select_user]
-
-			if @uID_list
-				# insert each data to Plan database
-				@uID_list.each do |uID|
-					@assign = Plan.create(time_plan)
-					user = User.find(uID.to_i)
-					user.plans << @assign
-				end
-				if @assign.save
-					#if creation is successful, show up 'successful' message
-					flash[:notice] = "Assign shift successfully"
-					redirect_to edit_dashboard_path(@dep)
-				else
-					render 'edit'
-				end
+			new_time_out = (@calender+' '+(params[:time].split[2])).to_time
+			new_time_in = (@calender+' '+(params[:time].split[0])).to_time
+			tomorrow = ["00:00","00:30","01:00","01:30","02:00","02:30","03:00","03:30","04:00","04:30","05:00","05:30","06:00","06:30","07:00","07:30","08:00","08:30"]
+			if tomorrow.include?(params[:time].split[2])
+				new_time_out += (3600*24)
 			end
+			if @uID_list
+				@uID_list.each do |uID|
+					@all_user_plan = Plan.find_by_user_id(uID) # get database
+					if @all_user_plan != nil
+						# insert each data to Plan database
+						if (@all_user_plan.time_in > new_time_out && @all_user_plan.time_in > new_time_in) || (@all_user_plan.time_out < new_time_in && @all_user_plan.time_out < new_time_out)
+							@assign = Plan.create(time_plan)
+							user = User.find(uID.to_i)
+							user.plans << @assign
+							if @assign.save
+								#if creation is successful, show up 'successful' message
+								flash[:notice] = "Assign shift successfully"
+								#redirect_to edit_dashboard_path(@dep)
+							else
+								render 'edit'
+							end
+						# update each data to Plan database		
+						elsif (@all_user_plan.time_in < new_time_out && @all_user_plan.time_in > new_time_in) || (@all_user_plan.time_out > new_time_in && @all_user_plan.time_out < new_time_out)
+							if @all_user_plan.update_attributes(time_plan) 
+						      flash[:notice] = "Update shift successfully"
+						      #redirect_to edit_dashboard_path(@dep)
+						    else
+						      render 'edit'
+						    end
+						else
+							flash[:notice] = "Can't assign shift"
+						end
+					# create data Plan if don't have anything in database
+					else
+						@assign = Plan.create(time_plan)
+						user = User.find(uID.to_i)
+						user.plans << @assign
+						if @assign.save
+							#if creation is successful, show up 'successful' message
+							flash[:notice] = "Assign shift successfully"
+							#redirect_to edit_dashboard_path(@dep)
+						else
+							render 'edit'
+						end
+					end
+				end
+				redirect_to edit_dashboard_path(@dep)
+			end
+			@status = false
 		end
 
 		# time
@@ -70,9 +105,8 @@ class DashboardsController < ApplicationController
 
 		@default_select = "00:00 - 09:00"
 		
-		@calender = params[:emp][:date]
-		@status = false
 		if (params.key?("choose"))
+			@calender = params[:emp][:date]
 			@status = true
 		end
 
