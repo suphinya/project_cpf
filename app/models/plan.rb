653be240792c5a66e02e54bb810dcd365939e1f0
Onpinya Phokhahutthakosol <ryukoki2000@gmail.com>
2021-11-14 07:11:57 -0500

    Pre version1st update1st

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,4 +2,10 @@
 class Plan < ActiveRecord::Base
 	belongs_to :user
 	has_many :actuals
+
+	validates :user_id, :presence=>true
+	validates :time_in, :presence=>true
+	validates :time_out, :presence=>true
+	validates :date, :presence=>true
+	validates :OT, :presence=>true
 end
