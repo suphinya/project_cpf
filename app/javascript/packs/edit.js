08154c1d502b65db4810d29a74255533b358dc04
ThatphumCpre <s6201012620112@email.kmutnb.ac.th>
2021-11-21 08:18:46 -0500

    Added when change date change date_out same as date_in

--- app/javascript/packs/edit.js
+++ app/javascript/packs/edit.js
@@ -3,3 +3,24 @@ $(document).ready(function() {
      document.getElementById('submit-btn').click()
   });
 });
+
+document.getElementById("date_out_date_out").value = document.getElementById("date_in_date_in").value;
+
+
+//var date = document.getElementById("date_in_date_in").value;
+//var varDate = new Date(date); //dd-mm-YYYY
+//var today = new Date();
+
+//if(varDate >= today) {
+  //getElementById("mobile-cta").value;
+//}
+
+//$(function () {
+        //$("#chkPassport").click(function () {
+            //if ($(this).is(":checked")) {
+                //$("#multiple").show();
+            //} else {
+                //$("#multiple").hide();
+            //}
+        //});
+//});
